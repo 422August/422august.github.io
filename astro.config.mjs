08c9a5909ad8b1aedbac21fbecefb7f890b1c369
August980422 <youxunzhou61@gmail.com>
2026-07-24 19:38:10 +0800

    rm

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
-const site = "https://422august.github.io/blogs";
+const site = "https://422august.github.io";
 
 export default defineConfig({
   site,
