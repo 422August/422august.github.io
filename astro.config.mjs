d469966bdf0d5552126ead018ce9a7c677a36088
August <177104126+422August@users.noreply.github.com>
2026-07-31 19:23:24 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
-const site = "https://august0422.tech";
+const site = "https://422august.github.io";
 
 export default defineConfig({
   site,
